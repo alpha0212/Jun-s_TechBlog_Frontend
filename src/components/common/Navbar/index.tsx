3de50c0ff6ed95379424477a68c101e4ebdf3472
alpha0212 <qkrcjfgml321@naver.com>
2022-09-25 21:22:44 +0900

    Add: Login Register Menu

--- src/components/common/Navbar/index.tsx
+++ src/components/common/Navbar/index.tsx
@@ -6,10 +6,13 @@ export const Navbar: React.FC = () => {
   return (
     <S.NavbarContainer>
       <S.NavbarObjectNavbar>
-        <S.NavbarLogoContainer>
+        <S.NavbarLogoContainer to="/">
           <S.NavbarLogo>J</S.NavbarLogo>
         </S.NavbarLogoContainer>
-        <S.NavbarLogoText>'s Tech Blog</S.NavbarLogoText>
+        <S.NavbarLogoText to="/">'s Tech Blog</S.NavbarLogoText>
+        <S.AuthPageContainer>
+          <S.AuthPage to="/auth/login">로그인 | 회원가입</S.AuthPage>
+        </S.AuthPageContainer>
       </S.NavbarObjectNavbar>
     </S.NavbarContainer>
   );
--- src/components/common/Navbar/styled.ts
+++ src/components/common/Navbar/styled.ts
@@ -1,4 +1,5 @@
 import styled from "@emotion/styled";
+import { Link } from "react-router-dom";
 
 export const NavbarContainer = styled.div`
   width: 100%;
@@ -13,9 +14,12 @@ export const NavbarObjectNavbar = styled.div`
   display: flex;
 `;
 
-export const NavbarLogoContainer = styled.div`
-  width: 4rem;
+export const NavbarLogoContainer = styled(Link)`
+  width: 4%;
   padding-bottom: 2rem;
+  text-decoration: none;
+  color: black;
+  float: left;
 `;
 
 export const NavbarLogo = styled.div`
@@ -23,17 +27,29 @@ export const NavbarLogo = styled.div`
   border-radius: 1.5rem;
   background-color: #dbdbdb;
   text-align: center;
-  padding-top: 0.4rem;
-  padding-bottom: 0.4rem;
+  padding-top: 0.3rem;
+  padding-bottom: 0.3rem;
   font-size: 3rem;
   font-weight: bold;
 `;
 
-export const NavbarLogoText = styled.div`
-  width: 20rem;
+export const NavbarLogoText = styled(Link)`
+  width: 30%;
   font-size: 3rem;
   font-weight: 600;
   color: #dbdbdb;
+  text-decoration: none;
   line-height: 3.7rem;
   margin-left: 0.5rem;
+`;
+
+export const AuthPageContainer = styled.div`
+  padding-top: 1.3rem;
+  width: 100%;
+`;
+
+export const AuthPage = styled(Link)`
+  float: right;
+  color: #dbdbdb;
+  text-decoration: none;
 `;
