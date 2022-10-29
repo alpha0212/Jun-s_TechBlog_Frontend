b0ea0381b0818add8392d4fa3bdd8d044f47c3e1
alpha0212 <qkrcjfgml321@naver.com>
2022-10-29 16:56:40 +0900

    Feature: Post Position

---
+++ src/components/common/Post/styled.ts
@@ -0,0 +1,17 @@
+import styled from "@emotion/styled";
+
+export const AllContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding-bottom: 5rem;
+`;
+
+export const PostContainer = styled.div`
+  margin: auto;
+`;
+
+export const PostPosition = styled.div`
+  width: 100rem;
+  height: 80rem;
+  background-color: white;
+`;
